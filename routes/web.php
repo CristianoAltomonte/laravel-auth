21c3c0b1c427e35ec33d314cdfa2e104bfc150ac
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2023-01-24 14:07:33 +0000

    inserimento vue

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Auth::routes();
+
+//Qui dobbiamo creare un modo per gestire le rotte sotto autenticazione
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin') //localhost:8080/admin/posts
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('index');
+        //controllers per crud
+        Route::resource('/posts', PostController::class);
+    });
+
+//Qui dobbiamo trovare un  modo per gestire tutte le rotte che non usano autenticazione
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where("any", ".*");
